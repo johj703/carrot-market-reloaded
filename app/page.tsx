8f307b2ec2962b01e0114d45f4a97f9fc8226461
Jo <johj703@nate.com>
2024-04-01 22:31:10 +0900

    카드 컴포넌트 초기 모습 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
 export default function Home() {
-  return <main className="bg-black"></main>;
+  return (
+    <main className="bg-slate-300 h-screen flex items-center justify-center">
+      <div>
+        <div>
+          <div>
+            <span>In transit</span>
+            <span>Coolblue</span>
+          </div>
+          <div />
+        </div>
+        <div>
+          <span>Today</span>
+          <span>9:30~10:30</span>
+        </div>
+        <div>
+          <div />
+          <div />
+        </div>
+        <div>
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span>Delivered</span>
+        </div>
+      </div>
+    </main>
+  );
 }
