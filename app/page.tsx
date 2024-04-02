9b1453c8b58c16f52a6cf5214703c7120fa88e81
Jo <johj703@nate.com>
2024-04-02 22:50:45 +0900

    invalid 요소를 넣으면 type에 설정했던 것들과 다르게 입력하면 invalid에 설정한 색이 나타나게 된다.

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen p-5 bg-slate-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm gap-2 p-5 bg-white shadow-lg rounded-3xl md:flex-row">
         <input
-          className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow"
-          type="text"
+          className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow invalid:bg-red-100"
+          type="email"
+          required
           placeholder="Search here!"
         />
         <button className="py-2 font-medium text-white transition-transform rounded-full outline-none active:scale-90 md:px-10 bg-black">
