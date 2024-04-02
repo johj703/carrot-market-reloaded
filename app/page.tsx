ecff49b1020213cce1bb7bf3e04ff9f9d1f7a7c2
Jo <johj703@nate.com>
2024-04-02 18:38:46 +0900

    input, button 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen p-5 bg-slate-100 ">
       <div className="w-full max-w-screen-sm p-5 bg-white shadow-lg rounded-3xl ">
+        <input type="text" placeholder="Search here!" />
+        <button>Search</button>
       </div>
     </main>
   );
