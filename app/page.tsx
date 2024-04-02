ab409c2392211d8f4cdf94acd957369ca472ca72
Jo <johj703@nate.com>
2024-04-02 23:02:34 +0900

    'invalid:focus:ring-red-500'과 같이 두 개의 modifier를 붙여서 사용할 수도 있다.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen p-5 bg-slate-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm gap-2 p-5 bg-white shadow-lg rounded-3xl md:flex-row">
         <input
-          className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow invalid:bg-red-100"
+          className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow invalid:focus:ring-red-500"
           type="email"
           required
           placeholder="Search here!"
