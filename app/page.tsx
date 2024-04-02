a941348c3b7ee8a346387fc579365d59c45b3bd1
Jo <johj703@nate.com>
2024-04-02 19:27:04 +0900

    브라우저의 크기가 달라지면 배경 색상을 변경하도록 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen p-5 bg-slate-100 sm:bg-red-100 md:bg-green-100">
+    <main className="flex items-center justify-center h-screen p-5 bg-slate-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm gap-2 p-5 bg-white shadow-lg rounded-3xl">
         <input className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow" type="text" placeholder="Search here!" />
         <button className="py-2 font-medium text-white transition-transform bg-black rounded-full outline-none active:scale-90 ">Search</button>
