6a913a7968b120eb12a56cab61e4e0e12041eb47
Jo <johj703@nate.com>
2024-04-02 17:59:38 +0900

    page.tsx 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
 export default function Home() {
   return (
-    <main className="bg-slate-300 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-2xl w-full">
-        <div className="flex justify-between items-center">
+    <main className="flex items-center justify-center h-screen p-5 bg-slate-300">
+      <div className="w-full p-5 bg-white shadow-lg rounded-2xl">
+        <div className="flex items-center justify-between">
           <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1">
+            <span className="-mb-1 font-semibold text-gray-600">
               In transit
             </span>
             <span className="text-4xl font-semibold">Coolblue</span>
           </div>
-          <div className="size-12 rounded-full bg-orange-400" />
+          <div className="bg-orange-400 rounded-full size-12" />
         </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full">
+        <div className="flex items-center gap-2 my-2">
+          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full ">
             Today
           </span>
           <span>9:30~10:30</span>
         </div>
         <div className="relative">
-          <div className="bg-gray-200 absolute rounded-full w-full h-2" />
-          <div className="bg-green-400 absolute rounded-full w-2/3 h-2" />
+          <div className="absolute w-full h-2 bg-gray-200 rounded-full" />
+          <div className="absolute w-2/3 h-2 bg-green-400 rounded-full" />
         </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600">
+        <div className="flex items-center justify-between mt-5 text-gray-600">
           <span>Expected</span>
           <span>Sorting center</span>
           <span>In transit</span>
