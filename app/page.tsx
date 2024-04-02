199fab13a9757a00fe3936bfafd2e787b9b0fb98
Jo <johj703@nate.com>
2024-04-02 18:51:09 +0900

    input, button css 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen p-5 bg-slate-100 ">
-      <div className="w-full max-w-screen-sm p-5 bg-white shadow-lg rounded-3xl ">
-        <input type="text" placeholder="Search here!" />
-        <button>Search</button>
+      <div className="flex flex-col w-full max-w-screen-sm gap-2 p-5 bg-white shadow-lg rounded-3xl">
+        <input className="w-full h-12 pl-5 bg-gray-200 rounded-full outline-none" type="text" placeholder="Search here!" />
+        <button className="py-2 font-medium text-white transition-transform bg-black rounded-full outline-none active:scale-90 focus:scale-90">Search</button>
       </div>
     </main>
   );
