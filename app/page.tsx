afe53b71cdbb58cbc63d2de5506105bfea8bc405
Jo <johj703@nate.com>
2024-04-02 21:42:55 +0900

    bg-gradient-to-tr from-첫 번째 색 to-두 번째 색을 추가하면 색이 첫 번째~두 번째 색이 하나의 버튼에서 섞여서 나타나게 된다

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen p-5 bg-slate-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm gap-2 p-5 bg-white shadow-lg rounded-3xl md:flex-row">
-        <input className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow" type="text" placeholder="Search here!" />
-        <button className="py-2 font-medium text-white transition-transform bg-black rounded-full outline-none active:scale-90 md:px-10">Search</button>
+        <input
+          className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow"
+          type="text"
+          placeholder="Search here!"
+        />
+        <button className="py-2 font-medium text-white transition-transform rounded-full outline-none active:scale-90 md:px-10 bg-gradient-to-tr from-cyan-500 to-purple-400">
+          Search
+        </button>
       </div>
     </main>
   );
