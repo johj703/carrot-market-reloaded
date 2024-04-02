cdd9a1e2497f204dfdbdee0ee69bd7ab47a6beca
Jo <johj703@nate.com>
2024-04-02 23:43:17 +0900

    peer 속성을 사용해서 peer가 유효하지 않다면, 배경색을 red-100으로 바뀌게 하도록 작성

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen p-5 bg-slate-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm gap-2 p-5 bg-white shadow-lg rounded-3xl md:flex-row">
         <input
-          className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 placeholder:drop-shadow invalid:focus:ring-red-500"
+          className="w-full h-10 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 placeholder:drop-shadow invalid:focus:ring-red-500 peer"
           type="email"
           required
           placeholder="Search here!"
         />
-        <button className="py-2 font-medium text-white transition-transform rounded-full outline-none active:scale-90 md:px-10 bg-black">
+        <button className="py-2 font-medium text-white transition-transform rounded-full outline-none active:scale-90 md:px-10 bg-black peer-invalid:bg-red-100">
           Search
         </button>
       </div>
