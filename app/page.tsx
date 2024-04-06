2817c63485dfa060cb5c1a72dca5b69f204cc6fd
Jo <johj703@nate.com>
2024-04-06 23:01:12 +0900

    빈 대화 목록 제거

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen p-5 bg-slate-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm p-5 bg-white shadow-lg rounded-3xl">
-        {["Jo", "Me", "You", "Yourself", ""].map((person, index) => (
+        {["Jo", "Me", "You", "Yourself"].map((person, index) => (
           <div key={index} className="flex items-center gap-5 ">
             <div className="bg-blue-400 rounded-full size-10" />
             <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
