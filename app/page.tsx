905eedb7fd23285d5f2b835c501bcd4c8e29a202
Jo <johj703@nate.com>
2024-04-06 23:09:27 +0900

    선택하고자 하는 대화창 컨테이너에 마우스를 올려 놓으면 이름이 빨간색으로 변하도록 hover 이벤트 작성

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
     <main className="flex items-center justify-center h-screen p-5 bg-slate-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex flex-col w-full max-w-screen-sm p-5 bg-white shadow-lg rounded-3xl">
         {["Jo", "Me", "You", "Yourself"].map((person, index) => (
-          <div key={index} className="flex items-center gap-5 ">
+          <div key={index} className="flex items-center gap-5 group">
             <div className="bg-blue-400 rounded-full size-10" />
-            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500">
               {person}
             </span>
             <div className="flex items-center justify-center text-white bg-red-500 rounded-full size-6">
