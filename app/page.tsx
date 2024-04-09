12cb18151f042bcb07b96af03bea911dadafb558
Jo <johj703@nate.com>
2024-04-09 18:23:36 +0900

    로그인 페이지 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    
+    <div>
+      <div>
+        <span>🥕</span>
+        <h1>당근</h1>
+        <h2>당근 마켓에 어서오세요!</h2>
+      </div>
+      <div>
+        <Link href="/create-account">시작하기</Link>
+      </div>
+      <span>이미 계정이 있나요?</span>
+      <Link href="/login">로그인</Link>
+    </div>
   );
 }
