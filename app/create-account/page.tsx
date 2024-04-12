aa362b564e5d4ccb48db685e3fed1df7016a85de
Jo <johj703@nate.com>
2024-04-12 18:01:43 +0900

    create-account 페이지 위에서 입력창까지 css 추가

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link"
 
 export default function CreateAccount() {
     return(
-        <div>
-            <div>
-                <h1>안녕하세요!</h1>
-                <h2>Fill in the form below to join!</h2>
+        <div className="flex flex-col gap-10 px-6 py-8">
+            <div className="flex flex-col gap-2 *:font-medium">
+                <h1 className="text-2xl">안녕하세요!</h1>
+                <h2 className="text-xl">Fill in the form below to join!</h2>
             </div>
-            <form>
+            <form className="flex flex-col gap-3">
                 <div>
-                    <input type="text" placeholder="Username" required />
+                    <input className="w-full h-10 bg-transparent border-none rounded-md focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500" type="text" placeholder="Username" required />
                     <span>Input error</span>
                 </div>
                 <button>Create account</button>
