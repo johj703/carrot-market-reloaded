49c3fe8a884990fdaa5c00ee7d8236d32425e619
Jo <johj703@nate.com>
2024-04-14 23:48:13 +0900

    form-index 컴포넌트 완성

--- components/form-index.tsx
+++ components/form-index.tsx
@@ -1,13 +1,29 @@
-export default function FormInput() {
+interface FormInputProps {
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+}
+
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+}: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
       <input
         className="w-full h-10 bg-transparent border-none rounded-md focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 placeholder:text-neutral-400"
-        type="text"
-        placeholder="Username"
-        required
+        type={type}
+        placeholder={placeholder}
+        required={required}
       />
-      <span className="font-medium text-red-500">Input error</span>
+      {errors.map((error, index) => (
+        <span key={index} className="font-medium text-red-500">
+          {errors}
+        </span>
+      ))}
     </div>
   );
 }
