2ba536d73b97e92fe7fd9bfa079042e46c0d106d
Jo <johj703@nate.com>
2024-04-16 19:54:36 +0900

    login 페이지에 필요한 구문 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ export default function LogIn() {
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">Fill in the form below to join!</h2>
+        <h2 className="text-xl">Log in with email and password.</h2>
       </div>
       <form className="flex flex-col gap-3">
         <FormInput type="email" placeholder="Email" required errors={[]} />
