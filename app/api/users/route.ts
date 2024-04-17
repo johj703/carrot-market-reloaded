44785291234ed32f3ada5f8fb55ebffc7f9f6608
Jo <johj703@nate.com>
2024-04-17 23:03:06 +0900

    POST function 작성

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,3 +5,8 @@ export async function GET(request: NextRequest) {
     ok: true,
   });
 }
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  return Response.json(data);
+}
