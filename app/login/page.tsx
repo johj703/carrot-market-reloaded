a4f8e3d9b0b7c0bd9580d588cd45120979cbb966
Jo <johj703@nate.com>
2024-04-20 14:03:08 +0900

    server action 동작하는지 handleForm 함수 작성해서 확인

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,26 +5,31 @@ import SocialLogin from "@/components/social-login";
 export default function LogIn() {
   async function handleForm() {
     "use server";
-    console.log("I run in the server!");
+    console.log("i run in the server!");
   }
   return (
-    <div className="flex flex-col gap-10 px-6 py-8">
+    <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
       <form action={handleForm} className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
         <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
           errors={[]}
         />
-      </form>
-      <span>
         <FormButton loading={false} text="Log in" />
-      </span>
+      </form>
       <SocialLogin />
     </div>
   );
