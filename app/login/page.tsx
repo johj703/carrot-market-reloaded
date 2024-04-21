289617d047c24665d4fee49a465f5886ed86c6cb
Jo <johj703@nate.com>
2024-04-21 22:08:51 +0900

    handleForm 작성해서 로그인 정보가 전달이 되는지 확인

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,13 +3,12 @@ import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
-  async function handleForm(formData: FormData) {
+  async function handleForm() {
     "use server";
-    console.log(formData.get("email"), formData.get("password"));
-    console.log("i run in the server!");
+    console.log("I run in the server!");
   }
   return (
-    <div className="flex flex-col gap-10 py-8 px-6">
+    <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
