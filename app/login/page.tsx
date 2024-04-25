ca4e30edc44f7a3dc36e7167eceaad44f781fab0
Jo <johj703@nate.com>
2024-04-25 22:42:27 +0900

    login/actions에 서버 컴포넌트에서 작동하는 handleForm을 옮기고 useFormState를 작성해서 action을 form에 적용

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,21 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 import { redirect } from "next/navigation";
+import { useFormState } from "react-dom";
+import { handleForm } from "./actions";
 
 export default function LogIn() {
-  async function handleForm(formData: FormData) {
-    "use server";
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    console.log("logged in!");
-    redirect("/");
-  }
+  const [state, action] = useFormState(handleForm, { potato: 1 } as any);
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form action={handleForm} className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="email"
           type="email"
