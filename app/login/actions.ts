181e71ff432e85d7f309f6057d5808092757593e
Jo <johj703@nate.com>
2024-04-26 23:44:10 +0900

    redirect 추가

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { redirect } from "next/navigation";
+
 export async function handleForm(prevState: any, formData: FormData) {
   console.log(prevState);
   await new Promise((resolve) => setTimeout(resolve, 5000));
+  redirect("/")
   return {
     errors: ["wrong password", "password too short"],
   };
