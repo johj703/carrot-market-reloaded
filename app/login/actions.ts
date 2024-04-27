e6790fa262d924fd7f363e5702710524c5b7aa14
Jo <johj703@nate.com>
2024-04-27 23:55:18 +0900

    sms 로그인 구조를 만드려면 이 구조로 가야한다!

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -5,7 +5,19 @@ import { redirect } from "next/navigation";
 export async function handleForm(prevState: any, formData: FormData) {
   console.log(prevState);
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  redirect("/")
+  redirect("/");
+  if (pS === null) {
+    // send sms
+    return {
+      sms: true,
+    };
+  }
+  if (sms == true) {
+    //check token
+    return {
+      ok: true,
+    };
+  }
   return {
     errors: ["wrong password", "password too short"],
   };
