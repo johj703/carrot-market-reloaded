53de3720dce819587b897c526692ad61b200ee84
Jo <johj703@nate.com>
2024-04-27 23:56:03 +0900

    Update actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -6,18 +6,6 @@ export async function handleForm(prevState: any, formData: FormData) {
   console.log(prevState);
   await new Promise((resolve) => setTimeout(resolve, 5000));
   redirect("/");
-  if (pS === null) {
-    // send sms
-    return {
-      sms: true,
-    };
-  }
-  if (sms == true) {
-    //check token
-    return {
-      ok: true,
-    };
-  }
   return {
     errors: ["wrong password", "password too short"],
   };
