042da3e83f1e0b4ed51d017f3e020b62a53b59fc
Jo <johj703@nate.com>
2024-05-02 23:42:26 +0900

    data에 usename, email, password, confirm_password를 formData.get으로 넣기

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,3 +1,10 @@
 "use server";
 
-export async function createAccount(prevState: any, formData: FormData) {}
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirm_password: formData.get("confirm_password"),
+  };
+}
