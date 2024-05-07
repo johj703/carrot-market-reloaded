1c031d838009759d9a066ee9bfabf0b37280d433
Jo <johj703@nate.com>
2024-05-07 23:24:28 +0900

    Update actions.ts

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,23 +1,31 @@
 "use server";
 import { z } from "zod";
 
-const checkUsername = (username: string) => 
-  !username.includes("potato");
+const checkUsername = (username: string) => !username.includes("potato");
 
-const checkPasswords = ({password, confirm_password}: password: string, confirm_password: string) => password === confirm_password
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
 
-const formSchema = z.object({
-  username: z.string({
-    invalid_type_error: "Username must be a string!",
-    required_error: "Where is my username???"
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Where is my username???",
+      })
+      .min(3, "Way too short!!")
+      .max(10, "That is too long!")
+      .refine(checkUsername, "No potatoes allowed!"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
   })
-  .min(3, "Way too short!!")
-  .max(10, "That is too long!")
-  .refine(checkUsername , "No potatoes allowed!"),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-}).refine(checkPasswords, "Both passwords should be the same!")
+  .refine(checkPasswords, "Both passwords should be the same!");
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -27,7 +35,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get("confirm_password"),
   };
   const result = formSchema.safeParse(data);
-  if(!result.success) {
+  if (!result.success) {
     return result.error.flatten();
   }
 }
