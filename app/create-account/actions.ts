4628c749ff3c1e3bd2ae42874a6c359cd2190df5
Jo <johj703@nate.com>
2024-05-08 18:21:07 +0900

    에러를 flatten으로 확인

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -36,6 +36,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   };
   const result = formSchema.safeParse(data);
   if (!result.success) {
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }
