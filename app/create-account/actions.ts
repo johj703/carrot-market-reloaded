07d572f009eb0048af0a3209cf7acb8b9d0fd795
Jo <johj703@nate.com>
2024-05-11 23:11:54 +0900

    username 객체에는 toLowerCase와 trim 추가, email 객체에는 toLowerCase만 추가
    toLowerCase는 User가 대문자를 입력해도 모든 것을 소문자로 변경해준다. trim은 User가 시작과 끝에 공백을 넣었을 때, 공백을 제거해 준다.

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -24,8 +24,10 @@ const formSchema = z
       })
       .min(3, "Way too short!!")
       .max(10, "That is too long!")
+      .toLowerCase()
+      .trim()
       .refine(checkUsername, "No potatoes allowed!"),
-    email: z.string().email(),
+    email: z.string().email().toLowerCase(),
     password: z
       .string()
       .min(10)
