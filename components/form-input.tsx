3c0c1858208cef551fa19cbfba3a3a40d024244d
Jo <johj703@nate.com>
2024-05-18 22:46:02 +0900

    type, placeholder, required를 ...rest에 담고 input 태그에 props로 전달하도록 작성

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -6,20 +6,16 @@ interface FormInputProps {
 }
 
 export default function FormInput({
-  type,
-  placeholder,
-  required,
-  errors = [],
   name,
+  errors = [],
+  ...rest
 }: FormInputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="flex flex-col gap-2">
       <input
         name={name}
         className="w-full h-10 transition bg-transparent border-none rounded-md focus:outline-none ring-2 focus:ring-4 ring-neutral-200 focus:ring-orange-500 placeholder:text-neutral-400"
-        type={type}
-        placeholder={placeholder}
-        required={required}
+        {...rest}
       />
       {errors.map((error, index) => (
         <span key={index} className="font-medium text-red-500">
