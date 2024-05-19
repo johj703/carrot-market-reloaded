33cd418b6b3f29834bc9e63acbe6ddcf4dff69d9
Jo <johj703@nate.com>
2024-05-19 20:26:37 +0900

    FormInput을 Input으로 수정

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
+import Input from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
 import { createAccount } from "./actions";
@@ -15,7 +15,7 @@ export default function CreateAccount() {
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <FormInput
+        <Input
           name="username"
           type="text"
           placeholder="Username"
@@ -24,14 +24,14 @@ export default function CreateAccount() {
           minLength={3}
           maxLength={10}
         />
-        <FormInput
+        <Input
           name="email"
           type="email"
           placeholder="Email"
           required
           errors={state?.fieldErrors.email}
         />
-        <FormInput
+        <Input
           name="password"
           type="password"
           placeholder="Password"
@@ -39,7 +39,7 @@ export default function CreateAccount() {
           required
           errors={state?.fieldErrors.password}
         />
-        <FormInput
+        <Input
           name="confirm_password"
           type="password"
           placeholder="Confirm Password"
