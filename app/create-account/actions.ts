d83d2f4f9f168f12c6938d0500cbc07af9442114
Jo <johj703@nate.com>
2024-05-20 18:49:13 +0900

    lib 디렉토리에 constants.ts 파일에 passwordRegex 정규식을 옮기고 password_min_length 생성

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,9 +1,6 @@
 "use server";
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from "@/lib/constants";
 import { z } from "zod";
-
-const passwordRegex = new RegExp(
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/
-);
 
 const checkUsername = (username: string) => !username.includes("potato");
 
@@ -22,8 +19,6 @@ const formSchema = z
         invalid_type_error: "Username must be a string!",
         required_error: "Where is my username???",
       })
-      .min(3, "Way too short!!")
-      .max(10, "That is too long!")
       .toLowerCase()
       .trim()
       .transform((username) => `🔥${username}🔥`)
@@ -31,12 +26,12 @@ const formSchema = z
     email: z.string().email().toLowerCase(),
     password: z
       .string()
-      .min(4)
+      .min(PASSWORD_MIN_LENGTH)
       .regex(
-        passwordRegex,
+        PASSWORD_REGEX,
         "A password must have lowercase, UPPERCASE, a number and special characters."
       ),
-    confirm_password: z.string().min(4),
+    confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPasswords, {
     message: "Both passwords should be the same!",
