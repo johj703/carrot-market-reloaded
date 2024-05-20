d102f558d613ab62d46b3547f730c696630b4015
Jo <johj703@nate.com>
2024-05-20 18:58:39 +0900

    login 컴포넌트에 formSchema 추가

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,5 +1,13 @@
 "use server";
 
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from "@/lib/constants";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email().toLowerCase(),
+  password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX),
+});
+
 export async function login(prevState: any, formData: FormData) {
   const data = {
     email: formData.get("email"),
