09813b4e0a12073a6a47e4d4c1708b1190401832
Jo <johj703@nate.com>
2024-05-20 18:38:07 +0900

    login 페이지에 data 추가

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,6 +1,10 @@
 "use server";
 
 export async function login(prevState: any, formData: FormData) {
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
   return {
     errors: ["wrong password", "password too short"],
   };
