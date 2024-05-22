daf8d329fba71ec14c7385508c676fb602ac8319
Jo <johj703@nate.com>
2024-05-22 21:33:39 +0900

    useFormState 추가

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -3,8 +3,11 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { smsVerification } from "./actions";
 
 export default function SMSLogin() {
+  const [state, dispatch] = useFormState(smsVerification, null);
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
