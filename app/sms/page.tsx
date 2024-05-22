680ec01b11f6ae538ac785c4956f5c3109e2c59f
Jo <johj703@nate.com>
2024-05-22 21:25:20 +0900

    FormButton, FormInput을 Button, Input으로 변경

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,5 +1,5 @@
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-index";
+import Button from "@/components/button";
+import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
 
 export default function SMSLogin() {
@@ -10,19 +10,14 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput
-          type="number"
-          placeholder="Phone number"
-          required
-          errors={[]}
-        />
-        <FormInput
+        <Input type="number" placeholder="Phone number" required errors={[]} />
+        <Input
           type="number"
           placeholder="Verification code"
           required
           errors={[]}
         />
-        <FormButton loading={false} text="Verify" />
+        <Button loading={false} text="Verify" />
       </form>
     </div>
   );
