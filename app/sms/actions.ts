60dc932307de06e79e3b9eed7b67dba88966a09a
Jo <johj703@nate.com>
2024-05-23 21:51:14 +0900

    phoneSchema 생성

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,3 +1,8 @@
 "use server";
 
+import { z } from "zod";
+import validator from "validator";
+
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+
 export async function smsLogin(prevState: any, formData: FormData) {}
