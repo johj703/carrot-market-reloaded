3487512ba45d84836aa53bd224a61f66395d6b85
Jo <johj703@nate.com>
2024-05-23 21:58:24 +0900

    coerce의 역할은 유저가 입력한 string을 number로 변환한다.

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -5,6 +5,8 @@ import validator from "validator";
 
 const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
 
+const tokenSchema = z.coerce.number();
+
 export async function smsLogin(prevState: any, formData: FormData) {
   console.log(typeof formData.get("token"));
 }
