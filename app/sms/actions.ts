13eabe39d2544b090c88f9877b6b9d202d61e249
Jo <johj703@nate.com>
2024-05-23 22:05:27 +0900

    Update actions.ts

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -9,4 +9,5 @@ const tokenSchema = z.coerce.number().min(100000).max(999999);
 
 export async function smsLogin(prevState: any, formData: FormData) {
   console.log(typeof formData.get("token"));
+  console.log(typeof tokenSchema.parse(formData.get("token")));
 }
