7ea833055c4ee19c114e282fac68a17829acf719
Jo <johj703@nate.com>
2024-05-23 21:55:00 +0900

    smsLogin 함수에 formData가 들어오면, 자동으로 string으로 변환되도록 작성

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -5,4 +5,6 @@ import validator from "validator";
 
 const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
 
-export async function smsLogin(prevState: any, formData: FormData) {}
+export async function smsLogin(prevState: any, formData: FormData) {
+  console.log(typeof formData.get("token"));
+}
