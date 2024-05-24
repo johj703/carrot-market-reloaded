8846bdb00115dbdbc8307dcba4aef4a9cdc3b174
Jo <johj703@nate.com>
2024-05-24 23:23:15 +0900

    console.log 삭제

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -7,7 +7,4 @@ const phoneSchema = z.string().trim().refine((phone) => validator.isMobilePhone(
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
-}
+export async function smsLogin(prevState: any, formData: FormData) {}
