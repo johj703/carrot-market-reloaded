4b6d11b6983cea57094d4fd025eded4c8916b65e
Jo <johj703@nate.com>
2024-05-29 13:32:24 +0900

    Phone Number를 입력하면 인증 코드를 입력하는 Input이 나타나도록 작성

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -19,13 +19,6 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input
-          name="phone"
-          type="text"
-          placeholder="Phone number"
-          required
-          errors={state.error?.formErrors}
-        />
         {state.token ? (
           <Input
             name="token"
@@ -35,8 +28,16 @@ export default function SMSLogin() {
             min={100000}
             max={999999}
           />
-        ) : null}
-        <Button text="Verify" />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
