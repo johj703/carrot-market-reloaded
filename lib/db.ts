00ba4ec9015592fa7be7c4458554ecd13c645eb5
Jo <johj703@nate.com>
2024-06-04 23:58:57 +0900

    필요 없는 코드 삭제

--- lib/db.ts
+++ lib/db.ts
@@ -2,17 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const user = await db.user.findMany({
-    where: {
-      username: {
-        contains: "est",
-      },
-    },
-  });
-  console.log(user);
-}
-
-test();
-
 export default db;
