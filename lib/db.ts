89f25134ca2fd4d7c4b2aec0a87ae9b23c3933a0
Jo <johj703@nate.com>
2024-06-04 23:57:44 +0900

    findMany를 사용해서 est라는 단어를 포함하고 있는 userName을 찾도록 작성

--- lib/db.ts
+++ lib/db.ts
@@ -3,9 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function test() {
-  const user = await db.user.create({
-    data: {
-      username: "test",
+  const user = await db.user.findMany({
+    where: {
+      username: {
+        contains: "est",
+      },
     },
   });
   console.log(user);
